9a8fb6f14fff4a920c264127ed3866516a24a3bc
Andrea-Mattia <andrea.mattia95@gmail.com>
2021-04-20 17:58:09 +0200

    added text & image by vue

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-console.log('JS Works');
-console.log('Vue', Vue);
+/**
+ * INTRO VUE
+ * 
+ * Descrizione
+ * 1. Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ * 2. Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+// new vue instance
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello Vue! (text inserted by Vue)',
+        image: 'https://picsum.photos/id/1002/500/300',
+    }
+});
